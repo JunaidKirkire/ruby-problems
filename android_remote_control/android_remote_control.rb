8aff64d5b7b874dcf911b657eaeb4e88cf128410
JunaidKirkire <junaidbkirkire@gmail.com>
2015-10-29 20:30:40 +0530

    Control mobile browser remotely using web services & adb

---
+++ android_remote_control/android_remote_control.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'open-uri'
+
+get '/' do
+  list_of_android_devices = `adb devices`
+  list_of_android_devices
+end
+
+get '/start' do
+  device_id = params[:device_id]
+  url_to_open = params[:url]
+  system("adb -s #{device_id} shell am set-debug-app --persistent com.android.chrome")
+  system("adb -s #{device_id} shell am start -n com.android.chrome/com.google.android.apps.chrome.Main -d '#{url_to_open}'")
+  url_to_open
+end
+
+get '/stop' do
+  device_id = params[:device_id]
+  system("adb -s #{device_id} shell am force-stop com.android.chrome")
+end
+
+get '/clean' do
+  device_id = params[:device_id]
+  system("adb -s #{device_id} shell pm clear com.android.chrome")
+end
